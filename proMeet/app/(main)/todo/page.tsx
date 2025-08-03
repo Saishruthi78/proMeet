2449cfbd84b7fead148ab31481652706e2312c12
Saishruthi Kethireddy <saishruthi@example.com>
2025-08-03 16:53:08 +0530

    initial commit

--- proMeet/app/(main)/todo/page.tsx
+++
@@ -1,9 +0,0 @@
-import TodoList from "@/components/TodoList";
-
-export default function TodoPage() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-200">
-      <TodoList />
-    </div>
-  );
-}
